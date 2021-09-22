eb0cff38b6dc470598ef121210e8cc1e167bff38
Vallab Karanam <k.vallab6@gmail.com>
2021-09-22 13:24:29 -0700

    initial commit

---
+++ public/precache-manifest.42aad43392ba2dec7cb1f977a28ffd69.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f268a4e5ce491aa85ab20d03c7fa34f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed32f72a59769cec9cc",
+    "url": "/static/css/0.10eff1aa.chunk.css"
+  },
+  {
+    "revision": "9903e3f14dcb81960c5e",
+    "url": "/static/css/4.6d344725.chunk.css"
+  },
+  {
+    "revision": "528a0061e86355337458",
+    "url": "/static/css/5.b957ddbe.chunk.css"
+  },
+  {
+    "revision": "5206e61623de0b07e4c4",
+    "url": "/static/css/6.a60aebec.chunk.css"
+  },
+  {
+    "revision": "c441dbdedc839ceefe8d",
+    "url": "/static/css/7.d37dee14.chunk.css"
+  },
+  {
+    "revision": "1a2377f2be438c8823eb",
+    "url": "/static/css/8.6b80cc0f.chunk.css"
+  },
+  {
+    "revision": "ee1d743d70bc1e306a04",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "aed32f72a59769cec9cc",
+    "url": "/static/js/0.99db273b.chunk.js"
+  },
+  {
+    "revision": "91111b7bfb0679ca2647",
+    "url": "/static/js/3.c968659f.chunk.js"
+  },
+  {
+    "revision": "9903e3f14dcb81960c5e",
+    "url": "/static/js/4.dfe4c245.chunk.js"
+  },
+  {
+    "revision": "528a0061e86355337458",
+    "url": "/static/js/5.e54bc7b1.chunk.js"
+  },
+  {
+    "revision": "5206e61623de0b07e4c4",
+    "url": "/static/js/6.b80f2762.chunk.js"
+  },
+  {
+    "revision": "c441dbdedc839ceefe8d",
+    "url": "/static/js/7.be8c049d.chunk.js"
+  },
+  {
+    "revision": "1a2377f2be438c8823eb",
+    "url": "/static/js/8.555f9e0c.chunk.js"
+  },
+  {
+    "revision": "ee1d743d70bc1e306a04",
+    "url": "/static/js/main.77109abe.chunk.js"
+  },
+  {
+    "revision": "010715fa4e044fede4c7",
+    "url": "/static/js/runtime-main.663cabc7.js"
+  }
+]);
